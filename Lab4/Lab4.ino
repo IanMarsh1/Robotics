753967ba36a1b2751d3f5aeadaa7c0b6cffa9987
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2023-04-10 19:00:00 -0400

    Update Lab4.ino

--- Lab4/Lab4.ino
+++ Lab4/Lab4.ino
@@ -1,3 +1,16 @@
+#include <Pololu3piPlus32U4.h>
+
+using namespace Pololu3piPlus32U4;
+
+const float MOTOR_BASE_SPEED = 300.0;
+const int MOTOR_MIN_SPEED = 30;
+
+unsigned long motorCm;
+unsigned long motorPm;
+
+Buzzer buzzer;
+Motors motors;
+
 void setup() {
   // put your setup code here, to run once:
 
