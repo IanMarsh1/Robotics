a0011d675fb1d1a12cf8e7476590e4283f0d2f88
Ian Marsh <60201744+IanMarsh1@users.noreply.github.com>
2023-05-08 11:12:21 -0400

    Update Final.ino

--- Final/Final/Final.ino
+++ Final/Final/Final.ino
@@ -2,57 +2,25 @@
 #include <Servo.h>
 using namespace Pololu3piPlus32U4;
 
-/**
- *Custom class to keep track of servo positions and the distance measured at them.
- */
-class ultrasonicReading
-{
-private:
-	double position;
-	double weight;
-	// Distances are inverted, bigger number means closer to an obstacle.
-	double firstDistance;
-	double secondDistance;
-	double thirdDistance;
+float servoPositions[] = {140, 110, 90, 70, 40};
+float servoWeights[] = {5, 6, 7, 6, 5};
+float firstDistance[] = {0, 0, 0, 0, 0};
+float secondDistance[] = {0, 0, 0, 0, 0};
+int NUM_HEAD_POSITIONS = 5;
 
-public:
-	// Constructor
-	ultrasonicReading(double position, double weight)
-	{
-		this->position = position;
-		this->weight = weight;
-		firstDistance = 0;
-		secondDistance = 0;
-		thirdDistance = 0;
-	}
-	// Bigger number means closer to an obstacle
-	void setDistance(double distance)
-	{
-		if (distance < 0)
-			distance = 0;
-		// 3 distances to take average of them. Oldest distance overwritten by newer distances.
-		firstDistance = secondDistance;
-		secondDistance = thirdDistance;
-		thirdDistance = distance;
-	}
-	double getPosition()
-	{
-		return position;
-	}
-	// Average the 3 most recent measured distances and multiply by the weight.
-	double getAdjustedDistance()
-	{
-		double averageDistance = (firstDistance + secondDistance + thirdDistance) / 3;
-		return averageDistance * weight;
-	}
-};
+int currentHeadPosition = 0;
+boolean headDirectionClockwise = true;
+unsigned long headCm;
+unsigned long headPm;
+const unsigned long HEAD_MOVEMENT_PERIOD = 240;
+boolean flag = false;
 
-double kp = 20;
-double ki = 0.00005;
-double kd = 0.00001;
-double kiTotal = 0;
-double error = 0;
-double priorError = 0;
+float kp = 50;
+float ki = 0.00005;
+float kd = 0.00001;
+float kiTotal = 0;
+float error = 0;
+float priorError = 0;
 
 // Hardware
 Encoders encoders;
@@ -66,22 +34,22 @@ const int ECHO_PIN = 18;
 const int SERVO_PIN = 20; 
 
 // goals
-double xGoals[] = {60};
-double yGoals[] = {60};
+const int NUMBER_OF_GOALS = 1;
+float xGoals[] = {90};
+float yGoals[] = {90};
 int currentGoal = 0;
 
 // Encoder configurations
 const float DISTANCE_BETWEEN_WHEELS = 8.5; // 8.5cm
-const unsigned long ENCODER_PERIOD = 30;   //  miliseconds
+const unsigned long PERIOD = 30;   //  miliseconds
 const int CLICKS_PER_ROTATION = 12;		   // DC motor 1 rotation = 12 state changes marked by encoders
 const float GEAR_RATIO = 75.81F;
 const float WHEEL_DIAMETER = 3.2;
 const int WHEEL_CIRCUMFERENCE = 10.0531;
-const float CM_PER_INCH = 2.54;
 
 // Encoder variables
-unsigned long encoderCm;
-unsigned long encoderPm;
+unsigned long currentMillis;
+unsigned long prevMillis;
 
 // Speed control variables
 const float MOTOR_BASE_SPEED = 75.0;
@@ -95,31 +63,15 @@ float Sl = 0.0;
 const float pi = 3.14159;
 float currentX = 0.0F; // in CM
 float currentY = 0.0F; // in CM
-float theta = pi/2;
-
-// Servo variables
-ultrasonicReading SERVO_POSITIONS[] = {
-	ultrasonicReading(140, 5),
-	ultrasonicReading(110, 6),
-	ultrasonicReading(90, 7),
-	ultrasonicReading(70, 6),
-	ultrasonicReading(40, 5)};
-
-int NUM_HEAD_POSITIONS = 5; 
-int currentServoPosition = 0;
-boolean headDirectionClockwise = true;
-unsigned long headCm;
-unsigned long headPm;
-const unsigned long HEAD_MOVEMENT_PERIOD = 250;
-boolean flag = false;
+float curTheta = pi/2;
 
 // Ultrasonic variables
 unsigned long USCurrentMills;
 unsigned long USPrevMills;
 const unsigned int ULTRASONIC_PERIOD = 10;
-double ultraDistance = 0;
-double MAX_DISTANCE = 30;
-const double MAX_DISTANCE_TEMP = 5;
+float ultraDistance = 0;
+float MAX_DISTANCE = 30;
+const float MAX_DISTANCE_TEMP = 2;
 
 void setup(){
 	// Invert motors
@@ -128,13 +80,13 @@ void setup(){
 
 	// Attach servo pin to servo
 	headServo.attach(SERVO_PIN);
-  headServo.write(90);
+  //headServo.write(90);
 	// Set ultrasonic pins
 	pinMode(ECHO_PIN, INPUT);
 	pinMode(TRIG_PIN, OUTPUT);
 
 	// Send servo to first position
-	headServo.write(SERVO_POSITIONS[currentServoPosition].getPosition());
+	headServo.write(servoPositions[currentHeadPosition]);
 
   Serial.begin(57600);
   delay(3000);
@@ -143,8 +95,7 @@ void setup(){
 
 void loop(){
 	// Run funtions until all goals have been reached
-	if (currentGoal < sizeof(xGoals) / sizeof(xGoals[0])) // Correctly count number of elements in array.
-	{
+	if (currentGoal < NUMBER_OF_GOALS){ // Correctly count number of elements in array.
 		moveHead();
 		ultraReadCm();
 		localization();
@@ -158,29 +109,29 @@ void moveHead(){
 	if (headCm > headPm + HEAD_MOVEMENT_PERIOD){
     // Decide next position and switch direction if reached either end
     if (headDirectionClockwise){
-      if (currentServoPosition >= NUM_HEAD_POSITIONS - 1){ // Correctly count number of elements in array
+      if (currentHeadPosition >= NUM_HEAD_POSITIONS - 1){ // Correctly count number of elements in array
         headDirectionClockwise = !headDirectionClockwise;
-        currentServoPosition--;
+        currentHeadPosition--;
       }
 
       else{
         // Continue clockwise
-        currentServoPosition++;
+        currentHeadPosition++;
       }
     }
     else{
-      if (currentServoPosition <= 0){
+      if (currentHeadPosition <= 0){
         // Counterclockwise and reached end
         headDirectionClockwise = !headDirectionClockwise;
-        currentServoPosition++;
+        currentHeadPosition++;
       }
       else{
         // Continue counterclockwise
-        currentServoPosition--;
+        currentHeadPosition--;
       }
     }
     // Move to new position
-    headServo.write(SERVO_POSITIONS[currentServoPosition].getPosition());
+    headServo.write(servoPositions[currentHeadPosition]);
 
     headPm = headCm;
   }
@@ -209,7 +160,7 @@ void ultraReadCm(){
     if(ultraDistance == 0) ultraDistance = MAX_DISTANCE;
 
     // Save distance to object (associate distance with position measured at)
-    SERVO_POSITIONS[currentServoPosition].setDistance(MAX_DISTANCE - ultraDistance);
+    setDistance(MAX_DISTANCE - ultraDistance);
 
     flag = false;
     USPrevMills = USCurrentMills;
@@ -218,29 +169,29 @@ void ultraReadCm(){
 
 void localization(){
 	// Updates encoder information once every specified period of time
-	encoderCm = millis();
-	if (encoderCm < encoderPm + ENCODER_PERIOD)
-		return;
+	currentMillis = millis();
+	if (currentMillis > prevMillis + PERIOD){
 
-	// Inverted and reversed because going "backwards"
-	long countLeft = -encoders.getCountsAndResetRight();
-	long countRight = -encoders.getCountsAndResetLeft();
+    // Inverted and reversed because going "backwards"
+    long countLeft = -encoders.getCountsAndResetRight();
+    long countRight = -encoders.getCountsAndResetLeft();
 
-	// new measurements since previous
-	float Sl = (countLeft / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
-	float Sr = (countRight / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
+    // new measurements since previous
+    float Sl = (countLeft / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
+    float Sr = (countRight / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFERENCE);
 
-	float deltaS  = (Sl + Sr) / 2;					   // distance the centerpoint went
-	float changeTheta = (Sr - Sl) / DISTANCE_BETWEEN_WHEELS; // new rotation, change in theta
+    float deltaS  = (Sl + Sr) / 2;
+    float changeTheta = (Sr - Sl) / DISTANCE_BETWEEN_WHEELS;
 
-	float deltaX = deltaS  * cos(theta + (changeTheta / 2));
-	float deltaY = deltaS  * sin(theta + (changeTheta / 2));
+    float deltaX = deltaS  * cos(curTheta + (changeTheta / 2));
+    float deltaY = deltaS  * sin(curTheta + (changeTheta / 2));
 
-	theta = theta + changeTheta;
-	currentX = currentX + deltaX;
-	currentY = currentY + deltaY;
+    curTheta += changeTheta;
+    currentX += deltaX;
+    currentY += deltaY;
 
-	encoderPm = encoderCm;
+    prevMillis = currentMillis;
+  }
 }
 
 void potentialFields(){
@@ -248,78 +199,58 @@ void potentialFields(){
 	float distance = distanceCalc(currentX, currentY, xGoals[currentGoal], yGoals[currentGoal]);
 
 	// Reached close enough to goal
-	if (distance < .5)
-	{
-		motors.setSpeeds(0, 0);
-		buzzer.play("C32");
-		delay(300);
+	if (distance < .5){
+    motors.setSpeeds(0, 0);
+    buzzer.play(F("l8 cdefgab>c"));
+    delay(3000);
 		currentGoal++;
 		kp = 45;
 		return;
 	}
 
 	// Calculate rotation error
-	float idealRotation = atan2(yGoals[currentGoal] - currentY, xGoals[currentGoal] - currentX);
-	float rotationError = idealRotation - theta;
-	rotationError = atan2(sin(rotationError), cos(rotationError));
-  error = rotationError;
-  double kpResult = kp * error;
+	float goalTheta = atan2(yGoals[currentGoal] - currentY, xGoals[currentGoal] - currentX);
+	float error = goalTheta - curTheta;
+	error = atan2(sin(error), cos(error));
+
+  float kpResult = kp * error;
   kiTotal += error;
-  if (kiTotal > 50){
-    kiTotal = 50;
-  }
-  else if (kiTotal < -50){
-    kiTotal = -50;
-  }
-  double kiResult = ki * kiTotal;
-  double kdResult = kd * (error - priorError);
+  if (kiTotal > 50) kiTotal = 50;
+  else if (kiTotal < -50) kiTotal = -50;
+  float kiResult = ki * kiTotal;
+  float kdResult = kd * (error - priorError);
   priorError = error;
 
-	rotationError = kpResult + kiResult + kdResult;
+	error = kpResult + kiResult + kdResult;
 
-	// ---- Potential fields of obsticles ----
-	double leftAdjustment = 0;
-	double rightAdjustment = 0;
-	double centerAdjustedDistance = 0;
+	float leftPush = 0;
+	float rightPush = 0;
+	float centerAdjustedDistance = 0;
 	// For each servo angle
-	for (int i = 0; i < sizeof(SERVO_POSITIONS) / sizeof(SERVO_POSITIONS[0]); i++)
-	{
-		// Get recorded distance, adjusted with the particular angle's weight
-		double adjustedDistance = SERVO_POSITIONS[i].getAdjustedDistance();
-		if (SERVO_POSITIONS[i].getPosition() < 90)
-		{
-			// Object to right, speed up right
-			rightAdjustment += adjustedDistance;
+	for (int i = 0; i < NUM_HEAD_POSITIONS; i++){
+		float adjustedDistance = getAdjustedDistance(i);
+
+		if (servoPositions[i] < 90){
+			rightPush += adjustedDistance;
 		}
-		else if (SERVO_POSITIONS[i].getPosition() > 90)
-		{
-			// Object to left, speed up left
-			leftAdjustment += adjustedDistance;
+		else if (servoPositions[i] > 90){
+			leftPush += adjustedDistance;
 		}
-		else
-		{
-			// Center 90° angle, decide after
+		else{
 			centerAdjustedDistance = adjustedDistance;
 		}
 	}
-	// Determine which way the measurement at 90° is applied (Turn away from side with more things)
-	if (rightAdjustment > leftAdjustment)
-		rightAdjustment += centerAdjustedDistance;
-	else
-		leftAdjustment += centerAdjustedDistance;
+	if (rightPush > leftPush) rightPush += centerAdjustedDistance;
+	else leftPush += centerAdjustedDistance;
 
-	// Sum up all factors to get wheel speeds
-	float leftSpeed = MOTOR_BASE_SPEED  - rotationError + leftAdjustment - rightAdjustment;
-	float rightSpeed = MOTOR_BASE_SPEED  + rotationError + rightAdjustment - leftAdjustment;
+  float push = leftPush - rightPush;
+	float leftSpeed = MOTOR_BASE_SPEED  - error + push;
+	float rightSpeed = MOTOR_BASE_SPEED  + error + -(push);
 
-	// Close to target.  Ignore a lot of sensor data, increase goal-seeking PID.
-	if (distance < 10)
-	{
+	if (distance < 10){
 		kp = 100;
 		MAX_DISTANCE = MAX_DISTANCE_TEMP;
-		// Slow down near target using quadratic-calculated distance. (The if prevents speeding up)
-		if (distance / 5 < 1)
-		{
+		if (distance / 5 < 1){
 			leftSpeed *= distance / 5;
 			rightSpeed *= distance / 5;
 		}
@@ -333,7 +264,7 @@ void potentialFields(){
   if (leftSpeed > MAX_SPEED)
     leftSpeed = MAX_SPEED;
   if (rightSpeed > MAX_SPEED)
-     rightSpeed = MAX_SPEED;
+    rightSpeed = MAX_SPEED;
 
 	motors.setSpeeds(rightSpeed, leftSpeed);
 }
@@ -345,3 +276,12 @@ float distanceCalc(float x1, float y1, float x2, float y2) {
   return sqrt(dx*dx + dy*dy);
 }
 
+void setDistance(float distance){
+		firstDistance[currentHeadPosition] = secondDistance[currentHeadPosition];
+		secondDistance[currentHeadPosition] = distance;
+}
+
+float getAdjustedDistance(int index){
+	float averageDistance = (firstDistance[index] + secondDistance[index]) / 2;
+	return averageDistance * servoWeights[index];
+}
